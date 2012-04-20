4e6a7b183762f283d0826d8cac8f87f8c06d458f
Philip Rideout <prideout@gmail.com>
2012-04-20 08:57:56 -0700

    progress

--- DeriveTorus.py
+++ DeriveTorus.py
@@ -8,29 +8,46 @@ from sympy.functions import sin,cos
 def VVF(*args):
     return Matrix(args)
 def DVVF(m, variable):
-    return Matrix([diff(m[i],variable) for i in xrange(len(m))])
+    return m.applyfunc(lambda f: diff(f,variable))
 def Print(label, vvf):
     print label + '\n' + '-' * len(label)
     print "x =", vvf[0]
     print "y =", vvf[1]
     print "z =", vvf[2]
     print
+def PrintPlot(vvf):
+    """Paste into Wolfram Alpha and enjoy"""
+    strings = tuple(map(str, vvf))
+    print "ParametricPlot3D[{%s, %s, %s}, {u,0,2pi}, {v,0,2pi}]" % strings
 def Simplify(m):
-    for i in xrange(len(m)):
-        m[i] = simplify(m[i])
+    m.simplify()
     return m
+def Normalized(m):
+    return Simplify(m / simplify(m.norm()))
 
-u, v = symbols('u v')
+u, v = symbols('u v', positive=True)
 
-def Sweep(curvePositions, curveNormals, crossSection):
-    """Takes three vector-valued functions:"""
-    """The first two arguments are functions of u"""
-    """The crossSection should be a function of v"""
-    curveTangents = DVVF(curvePositions,u)
-    curveBinormals = curveNormals.cross(curveTangents).transpose()
-    curveBinormals = Simplify(curveBinormals).normalized()
-    curveBasis = curveNormals.row_join(curveTangents).row_join(curveBinormals)
-    return curvePositions + curveBasis * crossSection
+def Sweep(sweepCurve, crossSection):
+    """Takes two vector-valued functions: """
+    """ - sweepCurve is a function of u   """
+    """ - crossSection is a function of v """
+
+    # Compute first-order and second-order derivatives:
+    d = DVVF(sweepCurve,u)
+    dd = DVVF(d,u)
+
+    # Perform Gram-Schmidt orthogonalization:
+    # Does NOT assume the sweep is an arc-length parameterization.
+    t = Normalized(d)
+    n = Normalized(dd - t * dd.dot(t))
+    b = Normalized(t.cross(n).transpose())
+
+    # Formulate the Frenet Frame:
+    curveBasis = t.row_join(n).row_join(b)
+
+    # Transform the cross section to the curve's space:
+    return Simplify(sweepCurve + curveBasis * crossSection)
+
 def NormalFunc(f):
     """Takes a vector-valued function of u and v"""
     """Computes formula for determining the surface normal at any point"""
@@ -42,11 +59,14 @@ def NormalFunc(f):
 # this prevents "Abs" expressions from cropping up in "normalize"
 r, R = symbols('r R', positive=True)
 
-positions = VVF(R*cos(u), R*sin(u), 0)
-normals = VVF(cos(u), sin(u), 0)
-crossSection = VVF(r*cos(v), 0, r*sin(v))
-torus = Sweep(positions, normals, crossSection)
+sweepCurve = VVF(R*cos(u), R*sin(u), 0)
+crossSection = VVF(0, -r*cos(v), r*sin(v))
+torus = Sweep(sweepCurve, crossSection)
 normals = NormalFunc(torus)
 
+print
 Print('Torus Surface', torus)
 Print('Torus Normals', normals)
+print
+PrintPlot(torus.subs({R: 3, r: 1}))
+print
